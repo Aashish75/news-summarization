0b1499d0fdc461d43a4c01af25010e1237f72648
Aashish75 <aashishmukund@gmail.com>
2025-02-17 14:11:53 -0700

    Adding all the files

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/header'; // Ensure the path matches the file name exactly, including case sensitivity
+import Column from './components/column';
+import './App.css'
+import Modal from './components/Modal';
+
+
+
+function App() {
+  return (
+    <div className ="appContainer">
+      <Header />
+      <Column />
+    </div>
+  );
+}
+
+export default App;
